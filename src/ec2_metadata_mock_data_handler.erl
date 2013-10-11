8fd60e1f64a19ff27a330739d9981225f5484106
Tommy Bishop <bishop.thomas@gmail.com>
2013-10-11 08:22:23 -0700

    refactor returning 404

--- src/ec2_metadata_mock_data_handler.erl
+++ src/ec2_metadata_mock_data_handler.erl
@@ -11,9 +11,7 @@ handle(Req, State) ->
     {Path, Req2} = cowboy_req:path(Req),
 
   case (string:str(binary:bin_to_list(Path), "/latest/")) of
-        0 ->
-            {ok, Req3} = cowboy_req:reply(404, [], <<"Not Found">>, Req2),
-            {ok, Req3, State};
+        0 -> return_404(Req2, State);
         _ ->
             Metadata = load_metadata_content(),
             {ok, Req3} = cowboy_req:reply(200, [{<<"content-type">>, <<"text/plain">>}], Metadata, Req),
@@ -22,6 +20,11 @@ handle(Req, State) ->
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+%% internal
+return_404(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(404, [], <<"Not Found">>, Req),
+    {ok, Req2, State}.
 
 load_metadata_content() ->
     {ok, Data} = file:read_file("metadata.json"),
