1d2e5d131ddccaf6309b529ee4b2c33a6e268986
Tommy Bishop <bishop.thomas@gmail.com>
2013-10-12 11:25:51 -0700

    add support for including '/' as appropriate

--- src/ec2_metadata_mock_data_handler.erl
+++ src/ec2_metadata_mock_data_handler.erl
@@ -26,20 +26,29 @@ terminate(_Reason, _Req, _State) ->
     ok.
 
 %% internal
+format_key(KeyData) ->
+  {Key, _} = KeyData,
+  case KeyData of
+      {_, true} ->
+          [Key, <<"\/\n">>];
+      {_, false} ->
+          [Key, <<"\n">>]
+  end.
+
 get_data_from_path(Path) ->
     RelativePath = relative_path(Path),
     AllData = load_metadata_content(),
 
     case (string:tokens(RelativePath, "/")) of
         [] ->
-          Keys = [element(1, X) || X <- element(1, AllData)],
-          list_to_binary(lists:map(fun(X) -> [X, <<"\n">>] end, Keys));
+            Keys = [{element(1, X), is_tuple(element(2, X))} || X <- element(1, AllData)],
+            list_to_binary(lists:map(fun(X) -> format_key(X) end, Keys));
         _ ->
             KeyData = ej:get(list_to_tuple(string:tokens(RelativePath, "/")), AllData),
             case (is_tuple(KeyData)) of
                 true ->
-                    Keys = [element(1, X) || X <- element(1, KeyData)],
-                    list_to_binary(lists:map(fun(X) -> [X, <<"\n">>] end, Keys));
+                    Keys = [{element(1, X), is_tuple(element(2, X))} || X <- element(1, KeyData)],
+                    list_to_binary(lists:map(fun(X) ->format_key(X) end, Keys));
                 false ->
                     KeyData
             end
