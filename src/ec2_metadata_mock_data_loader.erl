76586ec54f81da67c75f0b9739097e7bc706bf1f
Tommy Bishop <bishop.thomas@gmail.com>
2013-10-15 16:28:47 -0700

    more refactoring

--- src/ec2_metadata_mock_data_loader.erl
+++ src/ec2_metadata_mock_data_loader.erl
@@ -4,35 +4,39 @@
 
 data_from_path(Path) ->
     RelativePath = relative_path(Path),
-    AllData = load_metadata_content(),
+    AllData = all_data(),
 
     case (string:tokens(RelativePath, "/")) of
-        [] ->
-            Keys = [{element(1, X), is_tuple(element(2, X))} || X <- element(1, AllData)],
-            list_to_binary(lists:map(fun(X) -> format_key(X) end, Keys));
+        [] -> list_of_keys(AllData);
         _ ->
-            KeyData = ej:get(list_to_tuple(string:tokens(RelativePath, "/")), AllData),
-            case (is_tuple(KeyData)) of
+            KeyData = key_data(AllData, RelativePath),
+            case has_children(KeyData) of
                 true ->
-                    Keys = [{element(1, X), is_tuple(element(2, X))} || X <- element(1, KeyData)],
-                    list_to_binary(lists:map(fun(X) ->format_key(X) end, Keys));
+                    list_of_keys(KeyData);
                 false ->
                     KeyData
             end
     end.
 
-format_key(KeyData) ->
-  {Key, _} = KeyData,
-  case KeyData of
-      {_, true} ->
-          [Key, <<"\/\n">>];
-      {_, false} ->
-          [Key, <<"\n">>]
-  end.
-
-load_metadata_content() ->
+all_data() ->
     {ok, Json} = file:read_file(metadata_file()),
     jiffy:decode(Json).
+
+format_key(KeyData) ->
+  case KeyData of
+      {Key, true} -> [Key, <<"\/\n">>];
+      {Key, false} -> [Key, <<"\n">>]
+  end.
+
+has_children(Key) ->
+    is_tuple(Key).
+
+key_data(Data, Path) ->
+    ej:get(list_to_tuple(string:tokens(Path, "/")), Data).
+
+list_of_keys(Data) ->
+    Keys = [{element(1, X), has_children(element(2, X))} || X <- element(1, Data)],
+    list_to_binary(lists:map(fun(X) -> format_key(X) end, Keys)).
 
 metadata_file() ->
   case os:getenv("EC2_METADATA_FILE") of
